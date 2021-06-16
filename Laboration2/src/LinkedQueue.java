f012cf6c6c9f2b36ad8d3019c3dcb338eb50aea5
daskelito <henrik.heinze@hotmail.com>
2021-06-16 16:48:25 +0200

    hi

--- Laboration2/src/LinkedQueue.java
+++ Laboration2/src/LinkedQueue.java
@@ -1,43 +1,46 @@
 import java.util.LinkedList;
 
 public class LinkedQueue<T> implements Queue<T> {
-	private LinkedList<T> queue = new LinkedList<T>();
+    private LinkedList<T> queue = new LinkedList<T>();
 
 
-	
-public static void main(String[] args) {
-		LinkedQueue<Double> queue = new LinkedQueue<Double>();
-		for(double d = 10; d<40; d++) {
-			queue.add(d);
-		}
-		System.out.println("size="+queue.size()+", first element="+queue.element());
-		while(!queue.isEmpty()) {
-			System.out.print(queue.remove()+" ");
-		}
-	}
+    public static void main(String[] args) {
+        LinkedQueue<Double> queue = new LinkedQueue<Double>();
+        for (double d = 10; d < 40; d++) {
+            queue.add(d);
+        }
+        System.out.println("size=" + queue.size() + ", first element=" + queue.element());
+        while (!queue.isEmpty()) {
+            System.out.print(queue.remove() + " ");
+        }
+    }
 
-	@Override
-	public void add(T element) {
+    @Override
+    public void add(T element) {
+        queue.addLast(element);
+    }
 
-	}
+    @Override
+    public T remove() {
+        if (queue.size() == 0)
+            throw new QueueException("dequeue: Queue is empty");
+        return queue.removeFirst();
+    }
 
-	@Override
-	public T remove() {
-		return null;
-	}
+    @Override
+    public T element() {
+        if (queue.size() == 0)
+            throw new QueueException("peek: Queue is empty");
+        return queue.removeFirst();
+    }
 
-	@Override
-	public T element() {
-		return null;
-	}
+    @Override
+    public boolean isEmpty() {
+        return (queue.size() == 0);
+    }
 
-	@Override
-	public boolean isEmpty() {
-		return false;
-	}
-
-	@Override
-	public int size() {
-		return 0;
-	}
+    @Override
+    public int size() {
+        return queue.size();
+    }
 }
