46b1a20fab7778e1c87cdd871aa33e22a959132c
daskelito <henrik.heinze@hotmail.com>
2021-06-29 14:25:34 +0200

    hi

---
+++ Laboration8 games/src/Controller.java
@@ -0,0 +1,11 @@
+public class Controller {
+//	private Buffer<Game> buffer;
+	
+//	public Controller(Buffer<Game> buffer) {
+//		this.buffer = buffer;
+//		// Skapa instans av tråd och starta tråden 
+//	}
+	
+	// Inre klass vilken vilken hämtar Game-objekt ur buffer och visar objekten
+	// i TextWindow. Använd println-metoden i TextWindow.
+}
