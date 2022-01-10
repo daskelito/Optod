5298e1e26924cb367d17c07287571b991a78f083
daskelito <henrik.heinze@hotmail.com>
2022-01-10 13:31:12 +0100

    hi

--- Assignment2/src/MessageClient.java
+++ Assignment2/src/MessageClient.java
@@ -6,7 +6,9 @@ import java.net.UnknownHostException;
 import java.util.LinkedList;
 
 /**
- * MessageClient contains logic for the client side of the corresponding MessageServer.
+ * MessageClient contains logic for the client side of the corresponding MessageServer. Contains an input stream
+ * for information that is sent to the client. Also has a list of observers that listens to the client for changes
+ * in its data.
  *
  * @author Henrik Heinze
  * @version 1.0
@@ -16,7 +18,6 @@ public class MessageClient {
     private String host;
     private ObjectInputStream ois;
     private LinkedList<EventObserver> observers = new LinkedList<EventObserver>();
-
 
     /**
      * Constructor of the client that requires a host ip and a port on where to operate.
@@ -31,14 +32,26 @@ public class MessageClient {
         new Connection().start();
     }
 
+    /**
+     * Adds the specified eventObserver to the internal list of observers. Entry must implement EventObserver.
+     * @param eventObserver the specified eventObserver.
+     */
     public void registerObserver(EventObserver eventObserver) {
         observers.add(eventObserver);
     }
 
+    /**
+     * When this function is called, it notifies all the registered observers that change has occured.
+     * @param message
+     */
     public void alertObservers(Message message) {
         for (EventObserver eventObserver : observers) eventObserver.onEvent(message);
     }
 
+    /**
+     * Inner class that operates on a seperate thread and is instanciated once for each client. Constantly
+     * listens for new messages on the input stream and notifies the observers when a new message has been read.
+     */
     private class Connection extends Thread {
         @Override
         public void run() {
@@ -55,5 +68,4 @@ public class MessageClient {
         }
 
     }
-
 }
